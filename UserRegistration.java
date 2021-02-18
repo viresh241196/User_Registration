3bd60bd39d34137a51a3dc2ab8fcaa08a4cfee3f
viresh rawool <vireshrawool007@gmail.com>
2021-02-18 17:13:56 +0530

    Added: method to check the email samples

--- UserRegistration.java
+++ UserRegistration.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 public class UserRegistration {
     //method to check name is valid or not
     public static boolean nameValid(String name) {
-        String regx = "^[A-Z][a-zA-Z]{2,}";
+        String regx = "^[A-Z][a-zA-Z]{3,}";
         Pattern pattern = Pattern.compile(regx);
         Matcher matcher = pattern.matcher(name);
         return matcher.matches();
@@ -26,11 +26,19 @@ public class UserRegistration {
     }
     //rule3-min 8 char & atleast 1 upper case char and 1 number and exactly 1 special char
     public static boolean passwordValid(String password){
-        String regx = "[a-z[0-9]{1,}[A-Z]{1,}[!@#$%^&*]{1}]{8,}";
+        String regx = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()]).{8,}$";
         Pattern pattern = Pattern.compile(regx);
         Matcher matcher = pattern.matcher(password);
         return matcher.matches();
     }
+    //method to validate the given email samples
+    public static boolean emailSampleValid(String email){
+        String regx = "^abc[a-zA-Z0-9.+-]*@[a-z]*[.][a-z]{2,5}[.,a-z]{0,5}";
+        Pattern pattern = Pattern.compile(regx);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
     public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
         System.out.println("Enter first Name : ");
